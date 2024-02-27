f4064c0bd4a990c174d4d6e18257ed988524a170
Yahya Gilany <yahya.gilany@live.com>
2024-02-27 15:15:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Empty Template
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+- Remember to update the base in the `vite.config.js` file if you are deploying to a subdirectory.
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
+      <h1>IT3049C</h1>
+      <div className="card">
+        <p>
+        Edit <code>src/App.jsx</code> and save to update this view.
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
